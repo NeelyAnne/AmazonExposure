4d40be660ea26d2691e89bab035f4d0c9f5f4fdc
Neely Mann <55368415+NeelyAnne@users.noreply.github.com>
2020-07-05 20:22:58 -0400

    Update HiddenKey.java

--- aws-image-upload/src/main/java/com/talentpath/awsimageupload/config/HiddenKey.java
+++ aws-image-upload/src/main/java/com/talentpath/awsimageupload/config/HiddenKey.java
@@ -5,8 +5,7 @@ public class HiddenKey {
     private String privateKey;
 
     public HiddenKey() {
-        this.publicKey = "AKIAI5UIGAM5XEQQZMTA";
-        this.privateKey = "hTviBr+oVNV5jkFrOFvDgKVyxkoOToCGi6QWf91c";
+       
     }
 
     public String getPublicKey() {
